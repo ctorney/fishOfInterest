5136127da2d3dc8e810702c507bc3f4cf3e314d1
Colin Torney <colin.j.torney@gmail.com>
2016-09-30 14:21:53 +0100

    allow more positions than fish and filter at the end

--- linkTracks/main.cpp
+++ linkTracks/main.cpp
@@ -19,11 +19,12 @@ using namespace cv;
 // **************************************************************************************************
 struct track{
     int trackNo;
-    int absentFrames;
+    int length;
     float x;
     float y;
     float xp;
     float yp;
+    float vav;
     float vx;
     float vy;
     float ax;
@@ -42,8 +43,8 @@ int assignTracks(track* allTracks, float* dataXY,  int totalFish, int totalTrack
 
 int main( int argc, char** argv )
 {
-    string dataDir = "/home/ctorney/data/fishPredation/";
-    //string dataDir = "/home/ctorney/data/fishNick/";
+    //string dataDir = "/home/ctorney/data/fishPredation/";
+    string dataDir = "/media/ctorney/SAMSUNG/data/fishPredation/";
 
     // **************************************************************************************************
     // open the positions netcdf file
@@ -83,7 +84,8 @@ int main( int argc, char** argv )
     NcDim* frDim = inputFile.get_dim("frame");
     NcDim* iDim = inputFile.get_dim("fish");
     int totalFrames = frDim->size();
-    int totalFish = iDim->size();
+    int totalFish = iDim->size(); // number that the tracking can potentially find
+    int nFish = atoi(argv[2]); // real number of fish
     
     setUpNetCDF(&outputFile, totalFrames);
 
@@ -107,6 +109,8 @@ int main( int argc, char** argv )
     // **************************************************************************************************
     for (int f=0;f<totalFrames;f++)
     {
+       // cout<<f<<endl<<"==========="<<endl;
+
         float* dataXY = new float[totalFish*2];
         pxy->set_cur(f);
         pxy->get(dataXY, 1, totalFish, 2);
@@ -118,11 +122,16 @@ int main( int argc, char** argv )
         
         // main call to track linking
         totalTracks = assignTracks(allTracks, dataXY, totalFish, totalTracks, f);
+        //cout<<endl;
+ //       for (int i=0;i<totalFish;i++)
+ //           cout<<f<<" _ "<<dataXY[i*2]<<":"<<dataXY[i*2+1]<<":::"<<endl;
 
+        //cout<<endl<<f<<":";
         // output to netcdf file
         for (int i=0;i<totalFish;i++)
             if (allTracks[i].live)
             {
+                //cout<<allTracks[i].trackNo<<":";
                 // set the current entry
                 xy->set_cur(allTracks[i].trackNo, f, 0);
                 v->set_cur(allTracks[i].trackNo, f, 0);
@@ -141,6 +150,104 @@ int main( int argc, char** argv )
                 a->put(dataTRXY, 1, 1, 2);
             }
     }
+    //cout<<totalTracks<<endl;
+ //   return 0;
+
+    // thin the tracks by removing all the non moving entities
+    float *posOut = new float[totalTracks*2];
+    float *velOut = new float[totalTracks*2];
+    // first get average velocity for each track
+    float avVelocity[totalTracks];
+    int countVelocity[totalTracks];
+    for (int t=0;t<totalTracks;t++)
+    {
+        avVelocity[t]=0.0;
+        countVelocity[t]=0;
+    }
+    for (int f=0;f<totalFrames;f++)
+    {
+        v->set_cur(0, f, 0);
+        v->get(velOut, totalTracks, 1, 2);
+        xy->set_cur(0, f, 0);
+        xy->get(posOut, totalTracks, 1, 2);
+        for (int t=0;t<totalTracks;t++)
+            if (posOut[t*2]>0)
+            {
+                avVelocity[t]+=powf(velOut[t*2],2) + powf(velOut[t*2+1],2);
+                countVelocity[t]++;
+            }
+    }
+ //   for (int t=0;t<totalTracks;t++)
+  //      avVelocity[t]=avVelocity[t]/float(countVelocity[t]);
+    for (int f=0;f<totalFrames;f++)
+    {
+        xy->set_cur(0, f, 0);
+        xy->get(posOut, totalTracks, 1, 2);
+        int liveTracks = 0;
+        for (int t=0;t<totalTracks;t++)
+            if (posOut[t*2]>0)
+                liveTracks++;
+        if (liveTracks>nFish)
+        {
+            int trackNum[liveTracks];
+            int counter=0;
+            for (int t=0;t<totalTracks;t++)
+                if (posOut[t*2]>0)
+                    trackNum[counter++]=t;
+            // now delete the tracks with the lowest average speeds
+            // until we only have nFish left
+            while (liveTracks>nFish)
+            {
+                float minVelocity = 1.0e12;
+                int eraseThis = -1;
+                int erased = -1;
+                for (int t=0;t<liveTracks;t++)
+                {
+                    int thisTrack = trackNum[t];
+//                   cout<<thisTrack<<" "<<avVelocity[thisTrack]<<endl;
+                    if ((avVelocity[thisTrack]<=minVelocity)&(posOut[thisTrack*2]>0))
+                    {
+                        minVelocity=avVelocity[thisTrack];
+                        eraseThis = thisTrack;
+                        erased = thisTrack;
+                    }
+                }
+ //               cout<<f<<" "<<liveTracks<<" "<<nFish<<" "<<eraseThis<<endl;
+                posOut[eraseThis*2]=0;
+                for (int f1=0;f1<totalFrames;f1++)
+                {
+                    xy->set_cur(eraseThis, f1, 0);
+                    float* dataTRXY = new float[2];
+                    dataTRXY[0] = 0.0;
+                    dataTRXY[1] = 0.0;
+                    xy->put(dataTRXY, 1, 1, 2);
+                }
+                liveTracks--;
+            }
+        }
+    }
+    int countAllRealTracks[totalTracks];
+    for (int t=0;t<totalTracks;t++)
+        countAllRealTracks[t]=0;
+    for (int f=0;f<totalFrames;f++)
+    {
+        v->set_cur(0, f, 0);
+        v->get(velOut, totalTracks, 1, 2);
+        xy->set_cur(0, f, 0);
+        xy->get(posOut, totalTracks, 1, 2);
+        for (int t=0;t<totalTracks;t++)
+            if (posOut[t*2]>0)
+            {
+                countAllRealTracks[t]=1;
+                avVelocity[t]+=powf(velOut[t*2],2) + powf(velOut[t*2+1],2);
+                countVelocity[t]++;
+            }
+    }
+    int countAllReal = 0;
+    for (int t=0;t<totalTracks;t++)
+        countAllReal+=countAllRealTracks[t];
+    cout<<trialName<<" "<<countAllReal<<endl;
+
     return 0;
 }
 
@@ -179,26 +286,33 @@ int assignTracks(track* allTracks, float* dataXY,  int totalFish, int totalTrack
     // set initial values for positions
     // **************************************************************************************************
     int matchedTrack[totalFish];
+    int lengthTrack[totalFish];
+    float distToTrack[totalFish];
     bool doubleMatch[totalFish];
     for (int i=0;i<totalFish;i++)
     {
         matchedTrack[i]=-1;
+        lengthTrack[i]=0;
+        distToTrack[i]=0.0f;
         doubleMatch[i]=false;
     }
+ //   for (int j=0;j<totalFish;j++)
+ //       cout<<dataXY[j*2+0]<<" ";
+ //   cout<<endl;
 
     // **************************************************************************************************
     // nearest neighbour assignment for live tracks based on prediction from last time step
     // **************************************************************************************************
     for (int i=0;i<totalFish;i++)
     {
-        float minDist = 1e12;
+        float minDist = 50;
         if (!allTracks[i].live)
             continue;
         allTracks[i].empty = true;
         for (int j=0;j<totalFish;j++)
             if (dataXY[j*2+0]>0) 
             {
-                float dist = pow(allTracks[i].xp-dataXY[j*2+0],2) + pow(allTracks[i].yp-dataXY[j*2+1],2);
+                float dist = powf(pow(allTracks[i].xp-dataXY[j*2+0],2) + pow(allTracks[i].yp-dataXY[j*2+1],2),0.5);
                 if (dist<minDist)
                 {
                     minDist = dist;
@@ -212,10 +326,41 @@ int assignTracks(track* allTracks, float* dataXY,  int totalFish, int totalTrack
 
         if (!allTracks[i].empty)
         {
-            if (matchedTrack[allTracks[i].thisPos]<0)
-                matchedTrack[allTracks[i].thisPos] = i;
+            int j = matchedTrack[allTracks[i].thisPos];
+            int pos = allTracks[i].thisPos;
+            if (j<0)
+            {
+                matchedTrack[pos] = i;
+                distToTrack[pos]=minDist;
+                lengthTrack[pos] = allTracks[i].length;
+            }
             else
-                doubleMatch[allTracks[i].thisPos] = true;
+            {
+//                    cout<<allTracks[i].trackNo<<" j: ";
+//                    cout<<allTracks[j].trackNo<<" vavi: ";
+//                    cout<<allTracks[i].vav<<" vavj: ";
+//                    cout<<allTracks[j].vav<<" minDist: ";
+//                    cout<<minDist<<" exdist: ";
+//                    cout<<distToTrack[pos]<< endl;
+                // if j is static and further away then throw it out
+                if ((allTracks[j].vav<0.5)&&(distToTrack[pos]>minDist))
+                {
+                    matchedTrack[pos] = i;
+                    distToTrack[pos] = minDist;
+                    lengthTrack[pos] = allTracks[i].length;
+                    allTracks[j].empty = true;
+
+                }
+                else 
+                    // otherwise if i is moving or is closer we have a conflict
+                    if ((allTracks[i].vav>0.5)||(distToTrack[pos]>minDist))
+                        doubleMatch[pos] = true;
+                    else
+                    {
+               //         cout<<"should be here :"<<allTracks[i].trackNo<<endl;
+                        allTracks[i].empty = true;
+                    }
+            }
         }
 
 
@@ -233,7 +378,11 @@ int assignTracks(track* allTracks, float* dataXY,  int totalFish, int totalTrack
             if (doubleMatch[allTracks[i].thisPos])
                 allTracks[i].duplicate=true;
         }
-
+ /*           cout<<endl<<"PREDICTION:"<<allTracks[i].trackNo<<":"<<dataXY[allTracks[i].thisPos*2]<<":";
+            cout<<allTracks[i].xp<<":";
+            cout<<dataXY[allTracks[i].thisPos*2+1]<<":";
+            cout<<allTracks[i].yp<<endl;
+*/
 
     }
 
@@ -248,6 +397,7 @@ int assignTracks(track* allTracks, float* dataXY,  int totalFish, int totalTrack
             continue;
         if ((allTracks[i].empty)||(allTracks[i].duplicate))
         {
+          //  cout<<allTracks[i].trackNo<<" "<<allTracks[i].vav<<" "<<allTracks[i].length<<" e "<<allTracks[i].empty<<" d "<<allTracks[i].duplicate<<endl;
             allTracks[i].live = false;
             // delete the associated Kalman filter
             delete allTracks[i].KF;
@@ -268,9 +418,11 @@ int assignTracks(track* allTracks, float* dataXY,  int totalFish, int totalTrack
                     continue;
 
                 allTracks[j].thisPos=i;
+                allTracks[j].length=0;
                 allTracks[j].trackNo=trackCount;
                 allTracks[j].x=dataXY[i*2+0];
                 allTracks[j].y=dataXY[i*2+1];
+                allTracks[j].vav = 0.0f;
                 allTracks[j].vx = 0.0f;
                 allTracks[j].vy = 0.0f;
                 allTracks[j].ax = 0.0f;
@@ -324,6 +476,8 @@ int assignTracks(track* allTracks, float* dataXY,  int totalFish, int totalTrack
         allTracks[i].vy = allTracks[i].KF->statePre.at<float>(3);
         allTracks[i].ax = allTracks[i].KF->statePre.at<float>(4);
         allTracks[i].ay = allTracks[i].KF->statePre.at<float>(5);
+        allTracks[j].length++;
+        allTracks[i].vav += (pow(allTracks[i].vx,2) + pow(allTracks[i].vy,2));
 
         Mat_<float> measurement(2,1);
         measurement(0) = allTracks[i].x;
